9bc43fa8055dabe9227b86cbd8431a982994e3a0
jasito12345 <113585574+jasito12345@users.noreply.github.com>
2022-10-09 05:11:21 -0400

    Update pregunta6.hs

--- Problema6/pregunta6.hs
+++ Problema6/pregunta6.hs
@@ -1,17 +1,21 @@
+import System.IO
+sumar :: Double ->Double->Double
+sumar a b = a+b
+restar a b = a-b
+multiplicar a b = a*b
+dividir a b |b/=0 = a/b 
+            |otherwise =0
 main = do
-  print "Introduzca el primer numeros"
-  a <- getLine
-  print "Introduzca el segundo numeros"
-  b <- getLine
-  
-  let x = (read a :: Int)
-  let y = (read b :: Int)
-  
-  putStrLn "La suma de las dos cifras es:"
-  print (x + y)
-  putStrLn "La resta de las dos cifras es:"
-  print (x - y)
-  putStrLn "La multiplicacion de las dos cifras es:"
-  print (x * y)
-  putStrLn "La division de las dos cifras es:"
-  print (div x y)
+    print "Introduzca el primer numero"
+    x<-getLine 
+    print "Introduzca el segundo numero"
+    y<-getLine
+
+    putStrLn "La suma de las dos cifras es:"
+    print(sumar (read x) (read y))
+    putStrLn "La resta de las dos cifras es:"
+    print(restar  (read x) (read y))
+    putStrLn "La multiplicacion de las dos cifras es:"
+    print(multiplicar  (read x) (read x))
+    putStrLn "La division de las dos cifras es:"
+    print(dividir  (read x) (read y))
